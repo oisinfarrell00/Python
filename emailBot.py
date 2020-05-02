7043561a434d7bf3f666bff0b7b6de9ca8b58770
oisin farrell <48165055+oisinfarrell00@users.noreply.github.com>
2020-05-02 20:15:58 +0100

    Added input feature

--- emailBot.py
+++ emailBot.py
@@ -1,18 +1,22 @@
 import smtplib
 import ssl
 
+## Constant Port
 port = 465
-
-password = "paassword"
-
+sender_email = "example@tcd.ie"
+password = "password"
 context = ssl.create_default_context()
-
 server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
-server.login("address@gmail.com", password)
 
-sender_email = "address@gmail.com"
-receiver_email = "address1@gmail.com"
-message = "hello world!"
+## logging into your own email
+server.login(sender_email, password)
 
-server.sendmail(sender_email, receiver_email, message)
+receiver_email = input("Enter the email address of the person you want send this to: ")
+times_sent = input("Enter an amount of times you want to send it: ")
+message = input("Enter a message you want to send to them: ")
+print("Sending " + message + " to " + receiver_email + " " + times_sent + " times")
+for i in range(0, int(times_sent)):
+    message_sending = message + "\n \n \n \n \n \n " + "This is automated message " + (str(i) + 1) + " of " + times_sent
+    server.sendmail(sender_email, receiver_email, message_sending)
+
 print("done")
